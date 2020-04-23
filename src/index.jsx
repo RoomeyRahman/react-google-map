7c53b1e01f7c48a083ac6d24f295551d9d6eac32
RoomeyRahman <roomeyrahman@gmail.com>
2020-04-23 20:59:40 +0600

    project initialization

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>I am going to integrate google map in my react project</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
